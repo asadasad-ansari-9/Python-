cd642df0d8879be7933ead24a065a1b7622d197d
asadasad-ansari-9 <asadansari812611@gmail.com>
2026-08-30 20:25:57 +0530

    Add files via upload

---
+++ Practice_set_3.Practice_set_3.1/set_3.2_Q-6.py
@@ -0,0 +1,6 @@
+tup = (90,80,85)
+x,y,z = tup
+
+print(x)
+print(y)
+print(z)
---
+++ Practice_set_3.Practice_set_3.1/set_3.2_Q-8.py
@@ -0,0 +1,8 @@
+student =[
+    ("Asad", 90),
+    ("faiz",85),
+    ("aman",80)
+]
+
+for name,marks in student :
+    print(f"name :{name} \tmarks :{marks}")
