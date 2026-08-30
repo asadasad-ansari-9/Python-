ae1eab90290afab6b63b494870661fbc1620f1fd
asadasad-ansari-9 <asadansari812611@gmail.com>
2026-08-30 21:11:16 +0530

    Add files via upload

---
+++ Practice_set_2.1/set_2.1_Q-1.py
@@ -0,0 +1,8 @@
+num = int(input("Enter your number :"))
+
+if (num >= 1):
+    print("Your number is positive :",num)
+elif(num < 0):
+    print("Your number is negative :",num)
+if(num == 0):
+    print("Your number is zero :")        
